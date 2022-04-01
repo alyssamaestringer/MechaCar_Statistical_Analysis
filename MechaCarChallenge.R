bf4ac8f65bed2941dfc5c0a782ab40351831f729
Alyssa Stringer <alyssamstringer@gmail.com>
2022-03-31 23:56:48 -0500

    deliverable two finished

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,12 @@ mecha_car <- read_csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car))
 
+#import and read in suspension_coil
+sus_coil <- read_csv('Suspension_Coil.csv')
+
+#total summary frame
+total_summary <- sus_coil %>% group_by() %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#group each manufactoring lot by mean, median, variance and sd of PSI
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
