e235cee384babbd46a8322b7f17d9632e5dac2de
Alyssa Stringer <alyssamstringer@gmail.com>
2022-04-02 08:45:42 -0500

    updated R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,16 @@ total_summary <- sus_coil %>% group_by() %>% summarize(Mean=mean(PSI),Median=med
 #group each manufactoring lot by mean, median, variance and sd of PSI
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
+#t.test if PSI across all manufacturing lots is statistically different from the population mean
+t.test(log10(lot_summary$Mean),mu=mean(log10(total_summary$Mean)))
+
+#3 more arguments and subset
+#lot1
+t.test(subset(sus_coil, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+#lot2
+t.test(subset(sus_coil, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+#lot3
+t.test(subset(sus_coil, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+
+
+
